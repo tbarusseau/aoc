ccc832fbdd5f7afcacd7499f67d138dd504939d2
Thomas Barusseau <thomas.barusseau@the-endroad.com>
2021-12-01 09:17:11 +0100

    feat: y2021, day1

--- solutions/src/y2021/day1.rs
+++ solutions/src/y2021/day1.rs
@@ -2,24 +2,80 @@ use crate::solver::Solver;
 
 pub struct Day1;
 
-fn process_input(input: String) -> String {
-    input
+fn process_input(input: String) -> Vec<i32> {
+    input.lines().flat_map(str::parse).collect()
+}
+
+fn solve_part1(input: String) -> Box<dyn std::fmt::Display> {
+    let input = process_input(input);
+
+    let mut increased = 0;
+    for slice in input.windows(2) {
+        if let &[a, b] = slice {
+            if b > a {
+                increased += 1;
+            }
+        }
+    }
+
+    Box::new(increased)
+}
+
+fn solve_part2(input: String) -> Box<dyn std::fmt::Display> {
+    let input = process_input(input);
+
+    let mut increased = 0;
+    let mut previous = i32::MAX;
+
+    for slice in input.windows(3) {
+        if let &[a, b, c] = slice {
+            let current = a + b + c;
+            if current > previous {
+                increased += 1;
+            }
+            previous = current;
+        }
+    }
+
+    Box::new(increased)
 }
 
 impl Solver for Day1 {
     fn solve_part1(&self, input: String) -> Box<dyn std::fmt::Display> {
-        let _input = process_input(input);
-
-        Box::new("Part 1 not done yet".to_string())
+        solve_part1(input)
     }
 
     fn solve_part2(&self, input: String) -> Box<dyn std::fmt::Display> {
-        let _input = process_input(input);
-
-        Box::new("Part 2 not done yet".to_string())
+        solve_part2(input)
     }
 
     fn done(&self) -> bool {
-        false
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = r#"199
+200
+208
+210
+200
+207
+240
+269
+260
+263"#;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(7.to_string(), *solve_part1(INPUT.to_string()).to_string());
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(5.to_string(), *solve_part2(INPUT.to_string()).to_string());
     }
 }
