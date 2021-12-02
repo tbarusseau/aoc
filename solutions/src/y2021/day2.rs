87adfd67696d574c331be584c708a020ab355090
Thomas Barusseau <thomas.barusseau@the-endroad.com>
2021-12-02 08:55:40 +0100

    feat: y2021, day2

--- solutions/src/y2021/day2.rs
+++ solutions/src/y2021/day2.rs
@@ -2,39 +2,83 @@ use crate::solver::Solver;
 
 pub struct Day2;
 
-crate::impl_day!("2", false);
+crate::impl_day!("2", true);
 
-fn process_input(input: &str) -> &str {
-    input
+enum Instruction {
+    Forward(i32),
+    Down(i32),
+    Up(i32),
 }
 
-#[allow(unused)]
+fn process_input(input: &str) -> Vec<Instruction> {
+    use Instruction::*;
+
+    input
+        .lines()
+        .flat_map(|line| {
+            let mut split = line.split(' ');
+            let instruction = split.next().unwrap();
+            let value = split.next().unwrap().parse().unwrap();
+
+            match instruction {
+                "forward" => Some(Forward(value)),
+                "down" => Some(Down(value)),
+                "up" => Some(Up(value)),
+                _ => None,
+            }
+        })
+        .collect()
+}
+
 fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
     let input = process_input(input);
 
-    Box::new("Part 1 not done")
+    let mut pos = (0, 0);
+    input.iter().for_each(|instruction| match instruction {
+        Instruction::Forward(v) => pos.0 += v,
+        Instruction::Down(v) => pos.1 += v,
+        Instruction::Up(v) => pos.1 -= v,
+    });
+
+    Box::new(pos.0 * pos.1)
 }
 
-#[allow(unused)]
 fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
     let input = process_input(input);
 
-    Box::new("Part 2 not done")
+    let mut aim = 0;
+    let mut pos = (0, 0);
+    input.iter().for_each(|instruction| match instruction {
+        Instruction::Forward(v) => {
+            pos.0 += v;
+            pos.1 += aim * v;
+        }
+        Instruction::Down(v) => aim += v,
+        Instruction::Up(v) => aim -= v,
+    });
+
+    Box::new(pos.0 * pos.1)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = r#""#;
+    const INPUT: &str = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+";
 
     #[test]
     fn test_part1() {
-        assert_eq!(0.to_string(), *solve_part1(INPUT).to_string());
+        assert_eq!(150.to_string(), *solve_part1(INPUT).to_string());
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0.to_string(), *solve_part2(INPUT).to_string());
+        assert_eq!(900.to_string(), *solve_part2(INPUT).to_string());
     }
 }
