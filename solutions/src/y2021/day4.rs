ebcf97738dea4f3393582395d858ddc8905ed561
Thomas Barusseau <thomas.barusseau@protonmail.com>
2021-12-04 16:54:08 +0100

    feat: y2021, day4 and day5

--- solutions/src/y2021/day4.rs
+++ solutions/src/y2021/day4.rs
@@ -2,25 +2,145 @@ use crate::solver::Solver;
 
 pub struct Day4;
 
-crate::impl_day!("4", false);
+crate::impl_day!("4", true);
 
-fn process_input(input: &str) -> &str {
-    input
+use colored::Colorize;
+use grid::Grid;
+
+#[derive(Debug)]
+struct DrawOrder(Vec<i32>);
+
+struct Board(Grid<(i32, bool)>);
+
+impl std::fmt::Display for Board {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut out = String::new();
+
+        for i in 0..self.0.rows() {
+            self.0.iter_row(i).for_each(|(a, b)| {
+                if *b == true {
+                    out.push_str(&format!("{} ", a.to_string().blue()));
+                } else {
+                    out.push_str(&format!("{} ", a.to_string()));
+                }
+            });
+
+            out.push('\n');
+        }
+
+        write!(f, "{}", out)
+    }
 }
 
-#[allow(unused)]
-fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+fn process_input(input: &str) -> (DrawOrder, Vec<Board>) {
+    let mut lines = input.lines();
+    let mut grid: Grid<(i32, bool)> = Grid::new(5, 5);
+    grid.clear();
+    let mut row_index = 0;
+    let mut boards = vec![];
 
-    let res = "Part 1 not done";
+    let draw_order = DrawOrder(lines.next().unwrap().split(',').flat_map(str::parse).collect());
+    lines.next();
+
+    for line in lines {
+        if line.is_empty() {
+            continue;
+        }
+
+        let numbers = line.split(' ').flat_map(str::parse).collect::<Vec<i32>>();
+        grid.push_row(numbers.iter().map(|&n| (n, false)).collect());
+
+        row_index += 1;
+
+        if row_index == 5 {
+            boards.push(Board(grid.clone()));
+            grid.clear();
+
+            row_index = 0;
+        }
+    }
+
+    (draw_order, boards)
+}
+
+fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
+    let (ref mut draw_order, ref mut boards) = process_input(input);
+
+    let mut winning_board = None;
+    let mut last_draw = 0;
+
+    'outer: for draw in &draw_order.0 {
+        last_draw = *draw;
+
+        boards.iter_mut().for_each(|b| {
+            b.0.iter_mut().for_each(|(n, b)| {
+                if n == draw {
+                    *b = true;
+                }
+            })
+        });
+
+        for board in boards.iter() {
+            for i in 0..5 {
+                if board.0.iter_row(i).all(|(_, b)| { *b })
+                    || board.0.iter_col(i).all(|(_, b) | { *b })
+                    {
+                        winning_board = Some(board.0.clone());
+                        break 'outer;
+                    }
+            }
+        }
+    }
+
+    let sum_unmarked_numbers = winning_board.unwrap()
+        .iter()
+        .fold(0, |acc, (n, b)| if *b == true { acc } else { acc + n });
+
+    let res = sum_unmarked_numbers * last_draw;
     Box::new(res)
 }
 
-#[allow(unused)]
 fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+    let (ref mut draw_order, ref mut boards) = process_input(input);
 
-    let res = "Part 2 not done";
+    let mut winning_boards = vec![];
+    let mut last_winning_board = None;
+    let mut last_draw = 0;
+
+    'outer: for draw in &draw_order.0 {
+        last_draw = *draw;
+
+        boards.iter_mut().for_each(|b| {
+            b.0.iter_mut().for_each(|(n, b)| {
+                if n == draw {
+                    *b = true;
+                }
+            })
+        });
+
+        for (index, board) in boards.iter().enumerate() {
+            for i in 0..5 {
+                if board.0.iter_row(i).all(|(_, b)| { *b })
+                    || board.0.iter_col(i).all(|(_, b) | { *b })
+                    {
+                        if !winning_boards.contains(&index) {
+                            winning_boards.push(index);
+                        }
+
+                        if winning_boards.len() == boards.len() {
+                            last_winning_board = Some(board.0.clone());
+                            break 'outer;
+                        }
+                    }
+            }
+        }
+    }
+
+    let sum_unmarked_numbers = last_winning_board.unwrap()
+        .iter()
+        .fold(0, |acc, (n, b)| if *b == true { acc } else { acc + n });
+
+    let res = sum_unmarked_numbers * last_draw;
     Box::new(res)
 }
 
@@ -28,15 +148,33 @@ fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
 mod tests {
     use super::*;
 
-    const INPUT: &str = r#""#;
+    const INPUT: &str = r#"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+8  2 23  4 24
+21  9 14 16  7
+6 10  3 18  5
+1 12 20 15 19
+
+3 15  0  2 22
+9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+2  0 12  3  7"#;
 
     #[test]
     fn test_part1() {
-        assert_eq!(0.to_string(), *solve_part1(INPUT).to_string());
+        assert_eq!(4512.to_string(), *solve_part1(INPUT).to_string());
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0.to_string(), *solve_part2(INPUT).to_string());
+        assert_eq!(1924.to_string(), *solve_part2(INPUT).to_string());
     }
 }
--- solutions/src/y2021/day5.rs
+++ solutions/src/y2021/day5.rs
@@ -1,18 +1,114 @@
+use colored::Colorize;
+use grid::Grid;
+use regex::Regex;
+
 use crate::solver::Solver;
 
 pub struct Day5;
 
-crate::impl_day!("5", false);
+crate::impl_day!("5", true);
 
-fn process_input(input: &str) -> &str {
+struct VentsLine {
+    start: (i32, i32),
+    end: (i32, i32),
+}
+
+impl VentsLine {
+    fn is_vertical(&self) -> bool {
+        self.start.0 == self.end.0
+    }
+
+    fn is_horizontal(&self) -> bool {
+        self.start.1 == self.end.1
+    }
+}
+
+fn process_input(input: &str) -> Vec<VentsLine> {
+    let re = Regex::new(r"^(\d+),(\d+) -> (\d+),(\d+)").unwrap();
     input
+        .lines()
+        .map(|l| {
+            let cap = re.captures(l).unwrap();
+
+            VentsLine {
+                start: (cap[2].parse().unwrap(), cap[1].parse().unwrap()),
+                end: (cap[4].parse().unwrap(), cap[3].parse().unwrap()),
+            }
+        })
+        .collect()
+}
+
+fn step_towards(pos: &mut (i32, i32), end: &(i32, i32)) {
+    let mut multiplier = 1;
+
+    if pos.0 == end.0 {
+        if end.1 < pos.1 {
+            multiplier = -1;
+        }
+
+        pos.1 += multiplier;
+    } else if pos.1 == end.1 {
+        if end.0 < pos.0 {
+            multiplier = -1;
+        }
+
+        pos.0 += multiplier;
+    } else {
+        let x = if end.1 < pos.1 { -1 } else { 1 };
+        let y = if end.0 < pos.0 { -1 } else { 1 };
+
+        pos.0 += y;
+        pos.1 += x;
+    }
+}
+
+struct ResultGrid(Grid<i32>);
+
+impl std::fmt::Display for ResultGrid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut s = String::new();
+
+        for y in 0..self.0.rows() {
+            for x in 0..self.0.cols() {
+                let n = self.0[x][y];
+                if n == 0 {
+                    s.push('.');
+                } else if n >= 2 {
+                    s.push_str(&format!("{}", n).blue());
+                } else {
+                    s.push_str(&format!("{}", n));
+                }
+            }
+
+            s.push('\n');
+        }
+
+        write!(f, "{}", s)
+    }
 }
 
 #[allow(unused)]
 fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
     let input = process_input(input);
 
-    let res = "Part 1 not done";
+    let mut grid: Grid<i32> = Grid::new(1000, 1000);
+
+    for line in input {
+        if !line.is_horizontal() && !line.is_vertical() {
+            continue;
+        }
+
+        grid[line.start.1 as usize][line.start.0 as usize] += 1;
+        let mut pos = line.start;
+        while pos != line.end {
+            step_towards(&mut pos, &line.end);
+            grid[pos.1 as usize][pos.0 as usize] += 1;
+        }
+    }
+
+    let res = grid
+        .iter()
+        .fold(0, |acc, &n| if n >= 2 { acc + 1 } else { acc });
     Box::new(res)
 }
 
@@ -20,7 +116,20 @@ fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
 fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
     let input = process_input(input);
 
-    let res = "Part 2 not done";
+    let mut grid: Grid<i32> = Grid::new(1000, 1000);
+
+    for line in input {
+        grid[line.start.1 as usize][line.start.0 as usize] += 1;
+        let mut pos = line.start;
+        while pos != line.end {
+            step_towards(&mut pos, &line.end);
+            grid[pos.1 as usize][pos.0 as usize] += 1;
+        }
+    }
+
+    let res = grid
+        .iter()
+        .fold(0, |acc, &n| if n >= 2 { acc + 1 } else { acc });
     Box::new(res)
 }
 
@@ -28,15 +137,24 @@ fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
 mod tests {
     use super::*;
 
-    const INPUT: &str = r#""#;
+    const INPUT: &str = "0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2";
 
     #[test]
     fn test_part1() {
-        assert_eq!(0.to_string(), *solve_part1(INPUT).to_string());
+        assert_eq!(5.to_string(), *solve_part1(INPUT).to_string());
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0.to_string(), *solve_part2(INPUT).to_string());
+        assert_eq!(12.to_string(), *solve_part2(INPUT).to_string());
     }
 }
