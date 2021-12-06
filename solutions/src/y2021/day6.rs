0fa10c47c8dd5fbc80bcc4a79d1d805be79cacf0
Thomas BARUSSEAU <thomas.barusseau@protonmail.com>
2021-12-06 11:02:17 +0100

    feat: y2021, day6

--- solutions/src/y2021/day6.rs
+++ solutions/src/y2021/day6.rs
@@ -1,26 +1,71 @@
+use std::collections::HashMap;
+
 use crate::solver::Solver;
 
 pub struct Day6;
 
-crate::impl_day!("6", false);
+crate::impl_day!("6", true);
 
-fn process_input(input: &str) -> &str {
-    input
+struct Fishes(HashMap<usize, u64>);
+
+fn process_input(input: &str) -> Fishes {
+    let mut h = HashMap::new();
+
+    input.trim().split(',').for_each(|e| {
+        let n = e.parse::<usize>().unwrap();
+        *h.entry(n).or_insert(0) += 1;
+    });
+
+    Fishes(h)
 }
 
-#[allow(unused)]
 fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+    let mut input = process_input(input);
+    let mut day = 0;
 
-    let res = "Part 1 not done";
+    while day < 80 {
+        let mut new_fishes = HashMap::new();
+
+        input.0.iter().for_each(|(index, count)| {
+            if *index == 0 {
+                *new_fishes.entry(6).or_insert(0) += *count;
+                *new_fishes.entry(8).or_insert(0) += *count;
+            } else {
+                *new_fishes.entry(index - 1).or_insert(0) += *count;
+            }
+        });
+
+        input.0 = new_fishes;
+
+        day += 1;
+    }
+
+    let res = input.0.iter().fold(0, |acc, e| acc + e.1);
     Box::new(res)
 }
 
-#[allow(unused)]
 fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+    let mut input = process_input(input);
+    let mut day = 0;
 
-    let res = "Part 2 not done";
+    while day < 256 {
+        let mut new_fishes = HashMap::new();
+
+        input.0.iter().for_each(|(index, count)| {
+            if *index == 0 {
+                *new_fishes.entry(6).or_insert(0) += *count;
+                *new_fishes.entry(8).or_insert(0) += *count;
+            } else {
+                *new_fishes.entry(index - 1).or_insert(0) += *count;
+            }
+        });
+
+        input.0 = new_fishes;
+
+        day += 1;
+    }
+
+    let res = input.0.iter().fold(0, |acc, e| acc + e.1);
     Box::new(res)
 }
 
@@ -28,15 +73,15 @@ fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
 mod tests {
     use super::*;
 
-    const INPUT: &str = r#""#;
+    const INPUT: &str = "3,4,3,1,2";
 
     #[test]
     fn test_part1() {
-        assert_eq!(0.to_string(), *solve_part1(INPUT).to_string());
+        assert_eq!(5934.to_string(), *solve_part1(INPUT).to_string());
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0.to_string(), *solve_part2(INPUT).to_string());
+        assert_eq!(26984457539_u64.to_string(), *solve_part2(INPUT).to_string());
     }
 }
