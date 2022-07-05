101157d9492941d131caea580124fca0ede450f8
Thomas Barusseau <thomas.barusseau@the-endroad.com>
2022-07-06 00:14:26 +0200

    feat: 2021 d13, d14 first part

--- solutions/src/y2021/day13.rs
+++ solutions/src/y2021/day13.rs
@@ -1,38 +1,138 @@
+use std::collections::HashSet;
+
 use crate::solver::Solver;
 
 pub struct Day13;
 
-crate::impl_day!("13", false);
+crate::impl_day!("13", true);
 
-fn process_input(input: &str) -> &str {
-    input
+struct Instructions(Vec<(isize, bool)>);
+
+fn process_input(input: &str) -> (HashSet<(isize, isize)>, Instructions) {
+    let mut set = HashSet::new();
+    let mut lines = input.trim().lines();
+
+    while let Some(line) = lines.next() {
+        if line.is_empty() {
+            break;
+        }
+
+        let mut s = line.split(',');
+        let x = s.next().unwrap().parse::<isize>().unwrap();
+        let y = s.next().unwrap().parse::<isize>().unwrap();
+
+        set.insert((x, y));
+    }
+
+    let mut instructions = Vec::new();
+
+    for line in lines {
+        let index: isize = (line.chars().collect::<Vec<_>>()[13..])
+            .iter()
+            .collect::<String>()
+            .parse()
+            .unwrap();
+        let horizontal = line.starts_with("fold along x=");
+
+        instructions.push((index, horizontal));
+    }
+
+    (set, Instructions(instructions))
 }
 
-#[allow(unused)]
+fn fold(set: HashSet<(isize, isize)>, index: isize, horizontal: bool) -> HashSet<(isize, isize)> {
+    set.iter()
+        .map(|&(x, y)| {
+            if horizontal {
+                if x > index {
+                    (2 * index - x, y)
+                } else {
+                    (x, y)
+                }
+            } else {
+                if y > index {
+                    (x, 2 * index - y)
+                } else {
+                    (x, y)
+                }
+            }
+        })
+        .collect()
+}
+
 fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+    let (set, instructions) = process_input(input);
+    let (index, horizontal) = instructions.0[0];
 
-    let res = "Part 1 not done";
+    let set = fold(set, index, horizontal);
+
+    let res = set.iter().count();
     Box::new(res)
 }
 
-#[allow(unused)]
 fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+    let (mut set, instructions) = process_input(input);
+    let mut ins = instructions.0.iter();
 
-    let res = "Part 2 not done";
-    Box::new(res)
+    while let Some(ins) = ins.next() {
+        let &(index, horizontal) = ins;
+
+        set = fold(set, index, horizontal);
+    }
+
+    let mut max_x = 0;
+    let mut max_y = 0;
+
+    for &(x, y) in set.iter() {
+        if x > max_x {
+            max_x = x + 1;
+        }
+
+        if y > max_y {
+            max_y = y + 1;
+        }
+    }
+
+    let mut v = vec![format!("{}", " ".repeat(max_x as usize)); max_y as usize];
+
+    set.iter().for_each(|&(x, y)| {
+        v[y as usize].replace_range((x as usize)..(x as usize + 1), "X");
+    });
+
+    Box::new(format!("\n{}", v.join("\n")))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = r#""#;
+    const INPUT: &str = r#"
+6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5
+"#;
 
     #[test]
     fn test_part1() {
-        assert_eq!(0.to_string(), *solve_part1(INPUT).to_string());
+        assert_eq!(17.to_string(), *solve_part1(INPUT).to_string());
     }
 
     #[test]
--- solutions/src/y2021/day14.rs
+++ solutions/src/y2021/day14.rs
@@ -1,42 +1,110 @@
+use std::collections::HashMap;
+
 use crate::solver::Solver;
 
 pub struct Day14;
 
-crate::impl_day!("14", false);
+crate::impl_day!("14", true);
 
-fn process_input(input: &str) -> &str {
-    input
+fn process_input(input: &str) -> (String, HashMap<Vec<char>, String>) {
+    let mut lines = input.trim().lines();
+
+    let template = lines.next().unwrap().to_owned();
+    lines.next();
+
+    let mut h = HashMap::new();
+
+    for line in lines {
+        let mut split = line.split(" -> ");
+        let k = split.next().unwrap().chars().collect::<Vec<char>>();
+        let v = split.next().unwrap().to_owned();
+
+        h.insert(k, v);
+    }
+
+    (template, h)
 }
 
-#[allow(unused)]
+fn step(state: &str, rules: &HashMap<Vec<char>, String>) -> String {
+    let mut s = state
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(2)
+        .fold(String::new(), |mut s, e| {
+            let v = rules.get(e).unwrap();
+
+            s.push_str(&format!("{}{}", e[0], v));
+
+            s
+        });
+
+    s.push(state.chars().last().unwrap());
+
+    s
+}
+
+fn solve_for_n_cycles(input: &str, cycles: usize) -> usize {
+    let (mut polymer, h) = process_input(input);
+
+    for _ in 1..=cycles {
+        polymer = step(&polymer, &h);
+    }
+
+    let char_counts = polymer.chars().fold(HashMap::new(), |mut h, c| {
+        h.entry(c).and_modify(|v| *v += 1).or_insert(1);
+
+        h
+    });
+
+    let (_, most_frequent) = char_counts.iter().max_by_key(|e| e.1).unwrap();
+    let (_, least_frequent) = char_counts.iter().min_by_key(|e| e.1).unwrap();
+
+    most_frequent - least_frequent
+}
+
 fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
-
-    let res = "Part 1 not done";
-    Box::new(res)
+    Box::new(solve_for_n_cycles(input, 10))
 }
 
-#[allow(unused)]
 fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
-
-    let res = "Part 2 not done";
-    Box::new(res)
+    Box::new(solve_for_n_cycles(input, 40))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = r#""#;
+    const INPUT: &str = r#"
+NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C
+"#;
 
     #[test]
     fn test_part1() {
-        assert_eq!(0.to_string(), *solve_part1(INPUT).to_string());
+        assert_eq!(1588.to_string(), *solve_part1(INPUT).to_string());
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0.to_string(), *solve_part2(INPUT).to_string());
+        assert_eq!(
+            2188189693529_usize.to_string(),
+            *solve_part2(INPUT).to_string()
+        );
     }
 }
