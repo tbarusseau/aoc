287e049bcbdbd5108bca94a1825802aacaae9026
Thomas Barusseau <thomas.barusseau@the-endroad.com>
2022-07-05 17:38:23 +0200

    feat: day11, day12

--- solutions/src/y2021/day11.rs
+++ solutions/src/y2021/day11.rs
@@ -1,4 +1,4 @@
-use std::convert::TryFrom;
+use std::collections::HashSet;
 
 use crate::solver::Solver;
 
@@ -6,82 +6,117 @@ pub struct Day11;
 
 crate::impl_day!("11", true);
 
-fn process_input(input: &str) -> Vec<u32> {
+fn process_input(input: &str) -> Vec<Vec<u32>> {
     input
-        .trim()
-        .replace('\n', "")
-        .chars()
-        .flat_map(|c| c.to_digit(10))
-        .collect()
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| c.to_digit(10))
+                .flatten()
+                .collect::<Vec<u32>>()
+        })
+        .collect::<Vec<Vec<u32>>>()
 }
 
-const NEIGHBOUR_INDICES: &[isize] = &[-11, -10, -9, -1, 1, 9, 10, 11];
+const NEIGHBOUR_INDICES: &[(isize, isize)] = &[
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (-1, 0),
+    (1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+];
 
-fn increment_neighbours(input: &mut Vec<u32>, index: usize) {
-    for &neighbour_offset in NEIGHBOUR_INDICES {
-        if let Ok(i) = usize::try_from(index as isize + neighbour_offset) {
-            input.get_mut(i).map(|e| {
-                println!(
-                    "Incrementing neighbour at index {} from {} to {}",
-                    i,
-                    *e,
-                    *e + 1
-                );
-                *e += 1;
-            });
+fn modify_neighbours(input: &mut [Vec<u32>], pos: (isize, isize), f: fn(&mut u32)) {
+    let (x, y) = pos;
+
+    for (xi, yi) in NEIGHBOUR_INDICES {
+        if let Some(l) = input.get_mut((y + yi) as usize) {
+            if let Some(v) = l.get_mut((x + xi) as usize) {
+                f(v);
+            }
         }
     }
 }
 
-fn step(input: &mut Vec<u32>, flashes: &mut i32) {
-    // Increment all elements
-    input.iter_mut().for_each(|n| *n += 1);
+fn step(input: &mut Vec<Vec<u32>>) -> usize {
+    let mut flashes = 0;
 
-    for (index, n) in input.clone().iter().enumerate() {
-        if *n > 9 {
-            increment_neighbours(input, index);
+    // Increment all energy levels
+    for y in 0..10 {
+        for x in 0..10 {
+            input[y][x] += 1;
         }
     }
 
-    for n in input.iter_mut() {
-        if *n > 9 {
-            *n = 0;
-            *flashes += 1;
+    // Flash octopus
+    let mut flashed: HashSet<(usize, usize)> = HashSet::new();
+
+    loop {
+        let mut mutated = false;
+
+        for y in 0..10 {
+            for x in 0..10 {
+                let v = input[y][x];
+                if v > 9 {
+                    let pos = (x, y);
+
+                    if flashed.contains(&pos) {
+                        continue;
+                    }
+
+                    mutated = true;
+
+                    // Flash current octopus, increment neighbours
+                    flashed.insert(pos);
+                    flashes += 1;
+                    modify_neighbours(input, (pos.0 as isize, pos.1 as isize), |e| {
+                        *e += 1;
+                    });
+                }
+            }
+        }
+
+        if !mutated {
+            break;
         }
     }
+
+    // Reset flashed octopus
+    for (x, y) in flashed {
+        input[y][x] = 0;
+    }
+
+    return flashes;
 }
 
 fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
     let mut input = process_input(input);
     let mut flashes = 0;
 
-    let mut iter = input.chunks(10);
-    println!("Before any steps:");
-    while let Some(elems) = iter.next() {
-        println!("{:?}", elems);
-    }
-    println!("");
-
-    for i in 0..100 {
-        step(&mut input, &mut flashes);
-
-        let mut iter = input.chunks(10);
-        println!("After step {}:", i + 1);
-        while let Some(elems) = iter.next() {
-            println!("{:?}", elems);
-        }
-        println!("");
+    for _ in 0..100 {
+        flashes += step(&mut input);
     }
 
     Box::new(flashes)
 }
 
-#[allow(unused)]
 fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+    let mut input = process_input(input);
+    let mut steps = 0;
 
-    let res = "Part 2 not done";
-    Box::new(res)
+    loop {
+        step(&mut input);
+        steps += 1;
+
+        if input.iter().flatten().filter(|&v| *v == 0).count() == 100 {
+            break;
+        }
+    }
+
+    Box::new(steps)
 }
 
 #[cfg(test)]
@@ -107,6 +142,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(0.to_string(), *solve_part2(INPUT).to_string());
+        assert_eq!(195.to_string(), *solve_part2(INPUT).to_string());
     }
 }
--- solutions/src/y2021/day12.rs
+++ solutions/src/y2021/day12.rs
@@ -1,26 +1,107 @@
 use crate::solver::Solver;
+use petgraph::graphmap::UnGraphMap;
 
 pub struct Day12;
 
-crate::impl_day!("12", false);
+crate::impl_day!("12", true);
 
-fn process_input(input: &str) -> &str {
-    input
+enum Node {
+    Start,
+    End,
+    Small(String),
+    Big(String),
 }
 
-#[allow(unused)]
-fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+impl From<&str> for Node {
+    fn from(s: &str) -> Self {
+        match s {
+            "start" => Node::Start,
+            "end" => Node::End,
+            s => {
+                if s.len() != 2 {
+                    panic!("Doesn't have a size of two chars: {}", s);
+                }
 
-    let res = "Part 1 not done";
+                let mut chars = s.chars();
+                let c0 = chars.next().unwrap();
+                let c1 = chars.next().unwrap();
+
+                if (c0.is_uppercase() && !c1.is_uppercase())
+                    || (!c0.is_uppercase() && c1.is_uppercase())
+                {
+                    panic!("Chars have different cases: {}", s);
+                }
+
+                if c0.is_uppercase() {
+                    Node::Big(s.to_owned())
+                } else {
+                    Node::Small(s.to_owned())
+                }
+            }
+        }
+    }
+}
+
+fn process_input(input: &str) -> UnGraphMap<&str, ()> {
+    let mut edges = vec![];
+    for line in input.trim().lines() {
+        let v = line.split("-").collect::<Vec<&str>>();
+        let e1 = v[0];
+        let e2 = v[1];
+
+        edges.push((e1, e2));
+    }
+
+    UnGraphMap::<_, ()>::from_edges(edges)
+}
+
+fn is_small_cave(s: &str) -> bool {
+    return s.chars().next().unwrap().is_lowercase();
+}
+
+fn count(graph: &UnGraphMap<&str, ()>, small_caves: usize, seen: Vec<&str>, node: &str) -> usize {
+    let mut part = small_caves;
+
+    if node == "end" {
+        return 1;
+    }
+
+    if seen.contains(&node) {
+        if node == "start" {
+            return 0;
+        }
+
+        if is_small_cave(node) {
+            if small_caves == 1 {
+                return 0;
+            } else {
+                part = 1;
+            }
+        }
+    }
+
+    graph
+        .edges(node)
+        .map(|(_, e, _)| {
+            let mut v = seen.clone();
+            v.push(node);
+
+            return count(graph, part, v, e);
+        })
+        .sum()
+}
+
+fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
+    let graph = process_input(input);
+    let res = count(&graph, 1, vec![], "start");
+
     Box::new(res)
 }
 
-#[allow(unused)]
 fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+    let graph = process_input(input);
+    let res = count(&graph, 2, vec![], "start");
 
-    let res = "Part 2 not done";
     Box::new(res)
 }
 
@@ -28,15 +109,59 @@ fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
 mod tests {
     use super::*;
 
-    const INPUT: &str = r#""#;
+    const INPUT1: &str = r#"
+start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end
+"#;
+
+    const INPUT2: &str = r#"
+dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc
+"#;
+
+    const INPUT3: &str = r#"
+fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW
+"#;
 
     #[test]
     fn test_part1() {
-        assert_eq!(0.to_string(), *solve_part1(INPUT).to_string());
+        assert_eq!(10.to_string(), *solve_part1(INPUT1).to_string());
+        assert_eq!(19.to_string(), *solve_part1(INPUT2).to_string());
+        assert_eq!(226.to_string(), *solve_part1(INPUT3).to_string());
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0.to_string(), *solve_part2(INPUT).to_string());
+        assert_eq!(36.to_string(), *solve_part2(INPUT1).to_string());
     }
 }
