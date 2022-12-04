27bba1f9bd8481cc4b81bb48c1331bfc7ac96581
Thomas Barusseau <thomas.barusseau@the-endroad.com>
2022-12-04 12:02:15 +0100

    feat: y2022, day4

--- solutions/src/y2022/day4.rs
+++ solutions/src/y2022/day4.rs
@@ -1,26 +1,55 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
 use crate::solver::Solver;
 
 pub struct Day4;
 
-crate::impl_day!("4", false);
+crate::impl_day!("4", true);
 
-fn process_input(input: &str) -> &str {
-    input
+lazy_static! {
+    static ref RE: Regex = Regex::new(r"(\d+)-(\d+),(\d+)-(\d+)").unwrap();
 }
 
-#[allow(unused)]
-fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+fn parse(l: &str) -> (i32, i32, i32, i32) {
+    let caps = RE.captures(l).unwrap();
+    let s1 = i32::from_str_radix(caps.get(1).unwrap().as_str(), 10).unwrap();
+    let e1 = i32::from_str_radix(caps.get(2).unwrap().as_str(), 10).unwrap();
+    let s2 = i32::from_str_radix(caps.get(3).unwrap().as_str(), 10).unwrap();
+    let e2 = i32::from_str_radix(caps.get(4).unwrap().as_str(), 10).unwrap();
 
-    let res = "Part 1 not done";
+    (s1, e1, s2, e2)
+}
+
+fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
+    let mut res = 0;
+
+    for l in input.lines() {
+        let (s1, e1, s2, e2) = parse(l);
+
+        if (s1 <= s2 && e1 >= e2) || (s2 <= s1 && e2 >= e1) {
+            res += 1;
+        }
+    }
+
     Box::new(res)
 }
 
-#[allow(unused)]
 fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+    let mut res = 0;
 
-    let res = "Part 2 not done";
+    for l in input.lines() {
+        let (s1, e1, s2, e2) = parse(l);
+
+        if (s1 >= s2 && s1 <= e2)
+            || (s2 >= s1 && s2 <= e1)
+            || (e1 <= e2 && e1 >= s2)
+            || (e2 <= e1 && e2 >= s1)
+        {
+            res += 1;
+        }
+    }
+
     Box::new(res)
 }
 
@@ -28,15 +57,20 @@ fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
 mod tests {
     use super::*;
 
-    const INPUT: &str = r#""#;
+    const INPUT: &str = r#"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"#;
 
     #[test]
     fn test_part1() {
-        assert_eq!(0.to_string(), *solve_part1(INPUT).to_string());
+        assert_eq!(2.to_string(), *solve_part1(INPUT).to_string());
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0.to_string(), *solve_part2(INPUT).to_string());
+        assert_eq!(4.to_string(), *solve_part2(INPUT).to_string());
     }
 }
