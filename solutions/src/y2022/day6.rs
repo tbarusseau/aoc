d5ca336acf2dd7830832821ed1681f6230747ce0
Thomas Barusseau <thomas.barusseau@the-endroad.com>
2022-12-06 08:54:43 +0100

    feat: y2022, day6

--- solutions/src/y2022/day6.rs
+++ solutions/src/y2022/day6.rs
@@ -1,42 +1,94 @@
+use std::collections::{HashSet, VecDeque};
+
 use crate::solver::Solver;
 
 pub struct Day6;
 
-crate::impl_day!("6", false);
+crate::impl_day!("6", true);
 
-fn process_input(input: &str) -> &str {
-    input
+fn find_marker_index(input: &str, len: usize) -> Option<usize> {
+    let mut v = VecDeque::new();
+    let mut set = HashSet::new();
+
+    for (i, c) in input.chars().enumerate() {
+        set.clear();
+
+        v.push_back(c);
+
+        if v.len() > len {
+            v.pop_front();
+        }
+
+        for val in v.iter().cloned() {
+            set.insert(val);
+        }
+
+        if set.len() == len {
+            return Some(i + 1);
+        }
+    }
+
+    None
 }
 
-#[allow(unused)]
 fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
-
-    let res = "Part 1 not done";
-    Box::new(res)
+    Box::new(find_marker_index(input, 4).unwrap())
 }
 
 #[allow(unused)]
 fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
-
-    let res = "Part 2 not done";
-    Box::new(res)
+    Box::new(find_marker_index(input, 14).unwrap())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = r#""#;
-
     #[test]
     fn test_part1() {
-        assert_eq!(0.to_string(), *solve_part1(INPUT).to_string());
+        assert_eq!(
+            7.to_string(),
+            *solve_part1(r#"mjqjpqmgbljsphdztnvjfqwrcgsmlb"#).to_string()
+        );
+        assert_eq!(
+            5.to_string(),
+            *solve_part1(r#"bvwbjplbgvbhsrlpgdmjqwftvncz"#).to_string()
+        );
+        assert_eq!(
+            6.to_string(),
+            *solve_part1(r#"nppdvjthqldpwncqszvftbrmjlhg"#).to_string()
+        );
+        assert_eq!(
+            10.to_string(),
+            *solve_part1(r#"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"#).to_string()
+        );
+        assert_eq!(
+            11.to_string(),
+            *solve_part1(r#"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"#).to_string()
+        );
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0.to_string(), *solve_part2(INPUT).to_string());
+        assert_eq!(
+            19.to_string(),
+            *solve_part2(r#"mjqjpqmgbljsphdztnvjfqwrcgsmlb"#).to_string()
+        );
+        assert_eq!(
+            23.to_string(),
+            *solve_part2(r#"bvwbjplbgvbhsrlpgdmjqwftvncz"#).to_string()
+        );
+        assert_eq!(
+            23.to_string(),
+            *solve_part2(r#"nppdvjthqldpwncqszvftbrmjlhg"#).to_string()
+        );
+        assert_eq!(
+            29.to_string(),
+            *solve_part2(r#"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"#).to_string()
+        );
+        assert_eq!(
+            26.to_string(),
+            *solve_part2(r#"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"#).to_string()
+        );
     }
 }
