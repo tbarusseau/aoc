1fb14d19d777bb0c406119d7f4c586c6c6d8d359
Thomas BARUSSEAU <thomas.barusseau@swan.io>
2023-11-15 14:50:25 +0100

    feat: matrix utils

---
+++ solutions/src/utils/matrix.rs
@@ -0,0 +1,150 @@
+#[derive(Debug, PartialEq)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+
+impl<T> From<Vec<Vec<T>>> for Matrix<T> {
+    fn from(value: Vec<Vec<T>>) -> Self {
+        Matrix(value)
+    }
+}
+
+impl<T> Into<Vec<Vec<T>>> for Matrix<T> {
+    fn into(self) -> Vec<Vec<T>> {
+        self.0
+    }
+}
+
+impl<T> Matrix<T>
+where
+    T: Copy + std::cmp::PartialEq,
+{
+    pub fn rotate_right(&mut self) {
+        if self.0.len() == 0 || self.0.len() == 1 {
+            return;
+        }
+
+        self.transpose();
+
+        // Reverse each row
+        for i in 0..self.0.len() {
+            self.0[i].reverse();
+        }
+    }
+
+    pub fn transpose(&mut self) {
+        if self.0.len() == 0 || self.0.len() == 1 {
+            return;
+        }
+
+        for i in 0..self.0.len() {
+            for j in i..self.0[0].len() {
+                let tmp = self.0[i][j];
+                self.0[i][j] = self.0[j][i];
+                self.0[j][i] = tmp;
+            }
+        }
+    }
+
+    pub fn edge_matches(&self, other: &Self, direction: Direction) -> bool {
+        let len = self.0.len();
+
+        let [left, right] = match direction {
+            Direction::Up => [self.0[0].clone(), other.0[len - 1].clone()],
+            Direction::Down => [self.0[len - 1].clone(), other.0[0].clone()],
+            Direction::Right => {
+                let mut l = Vec::with_capacity(len);
+                let mut r = Vec::with_capacity(len);
+
+                for row in &self.0 {
+                    l.push(row[len - 1]);
+                }
+
+                for row in &other.0 {
+                    r.push(row[0]);
+                }
+
+                [l, r]
+            }
+            Direction::Left => {
+                let mut l = Vec::with_capacity(len);
+                let mut r = Vec::with_capacity(len);
+
+                for row in &self.0 {
+                    l.push(row[0]);
+                }
+
+                for row in &other.0 {
+                    r.push(row[len - 1]);
+                }
+
+                [l, r]
+            }
+        };
+
+        left == right
+    }
+}
+
+#[derive(Debug)]
+pub enum Direction {
+    Up,
+    Right,
+    Left,
+    Down,
+}
+
+impl Direction {
+    pub fn iterator() -> std::slice::Iter<'static, Direction> {
+        use Direction::*;
+
+        static DIRECTIONS: [Direction; 4] = [Up, Right, Left, Down];
+        DIRECTIONS.iter()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rotate_2x2() {
+        let mut m = Matrix::from(vec![vec![1, 2], vec![3, 4]]);
+
+        m.rotate_right();
+        assert_eq!(m, vec![vec![3, 1], vec![4, 2]].into());
+
+        m.rotate_right();
+        assert_eq!(m, vec![vec![4, 3], vec![2, 1]].into());
+
+        m.rotate_right();
+        assert_eq!(m, vec![vec![2, 4], vec![1, 3]].into());
+    }
+
+    #[test]
+    fn test_rotate_3x3() {
+        let mut m = Matrix::from(vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
+
+        m.rotate_right();
+        assert_eq!(m, vec![vec![7, 4, 1], vec![8, 5, 2], vec![9, 6, 3],].into());
+
+        m.rotate_right();
+        assert_eq!(m, vec![vec![9, 8, 7], vec![6, 5, 4], vec![3, 2, 1],].into());
+
+        m.rotate_right();
+        assert_eq!(m, vec![vec![3, 6, 9], vec![2, 5, 8], vec![1, 4, 7],].into());
+    }
+
+    #[test]
+    fn test_edge_matches() {
+        let a = Matrix::from(vec![vec![1, 2], vec![3, 4]]);
+        let b = Matrix::from(vec![vec![2, 1], vec![4, 3]]);
+
+        assert!(a.edge_matches(&b, Direction::Right));
+        assert!(b.edge_matches(&a, Direction::Left));
+
+        let a = Matrix::from(vec![vec![1, 2], vec![3, 4]]);
+        let b = Matrix::from(vec![vec![3, 4], vec![1, 2]]);
+
+        assert!(a.edge_matches(&b, Direction::Up));
+        assert!(b.edge_matches(&a, Direction::Down));
+    }
+}
--- solutions/src/y2020/day20.rs
+++ solutions/src/y2020/day20.rs
@@ -1,16 +1,83 @@
-use crate::solver::Solver;
+use std::{collections::HashMap, fmt::Display};
+
+use itertools::Itertools;
+use regex::Regex;
+
+use crate::{solver::Solver, utils::matrix::Matrix};
 
 pub struct Day20;
 
-crate::impl_day!("20", false);
+crate::impl_day!("20", true);
 
-fn process_input(input: &str) -> &str {
-    input
+struct Tile {
+    pub id: i32,
+    pub tile: Matrix<bool>,
 }
 
-#[allow(unused)]
+impl Display for Tile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Tile {}:\n", self.id)?;
+
+        for l in self.tile.0.iter() {
+            for b in l.iter() {
+                if *b {
+                    write!(f, "#")?;
+                } else {
+                    write!(f, ".")?;
+                }
+            }
+
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
+}
+
+fn process_input(input: &str) -> Vec<Tile> {
+    let re = Regex::new(r#"^Tile (\d+):$"#).expect("Invalid regex");
+    let mut tiles = Vec::with_capacity(144);
+
+    for split in input.split("\n\n") {
+        let mut lines = split.lines();
+
+        if let Some(first_line) = lines.next() {
+            let rest: Vec<&str> = lines.collect();
+
+            let id_str = re
+                .captures(&first_line)
+                .expect("doesn't match regex")
+                .get(1)
+                .expect("no capture group at index 1")
+                .as_str();
+            let id = i32::from_str_radix(id_str, 10).expect("tile id is not a valid i32");
+
+            let tile: Matrix<bool> = rest
+                .into_iter()
+                .map(|l| l.chars().map(|c| c == '#').collect())
+                .collect::<Vec<Vec<bool>>>()
+                .into();
+
+            let tile = Tile { id, tile };
+            tiles.push(tile);
+        }
+    }
+
+    tiles
+}
+
 fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
     let input = process_input(input);
+    let mut borders: HashMap<i32, Vec<Vec<bool>>> = HashMap::new();
+    let tile_len = input[0].tile.0.len();
+
+    for tile in input {
+        let key = tile.id;
+        let mut v: Vec<Vec<bool>> = vec![Vec::with_capacity(10); 4];
+
+        v.push(tile.tile.0[0].to_owned());
+        v.push(tile.tile.0[tile_len - 1].to_owned());
+    }
 
     Box::new("Part 1 not done")
 }
@@ -26,11 +93,122 @@ fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
 mod tests {
     use super::*;
 
-    const INPUT: &str = r#""#;
+    const INPUT: &str = r#"Tile 2311:
+..##.#..#.
+##..#.....
+#...##..#.
+####.#...#
+##.##.###.
+##...#.###
+.#.#.#..##
+..#....#..
+###...#.#.
+..###..###
+
+Tile 1951:
+#.##...##.
+#.####...#
+.....#..##
+#...######
+.##.#....#
+.###.#####
+###.##.##.
+.###....#.
+..#.#..#.#
+#...##.#..
+
+Tile 1171:
+####...##.
+#..##.#..#
+##.#..#.#.
+.###.####.
+..###.####
+.##....##.
+.#...####.
+#.##.####.
+####..#...
+.....##...
+
+Tile 1427:
+###.##.#..
+.#..#.##..
+.#.##.#..#
+#.#.#.##.#
+....#...##
+...##..##.
+...#.#####
+.#.####.#.
+..#..###.#
+..##.#..#.
+
+Tile 1489:
+##.#.#....
+..##...#..
+.##..##...
+..#...#...
+#####...#.
+#..#.#.#.#
+...#.#.#..
+##.#...##.
+..##.##.##
+###.##.#..
+
+Tile 2473:
+#....####.
+#..#.##...
+#.##..#...
+######.#.#
+.#...#.#.#
+.#########
+.###.#..#.
+########.#
+##...##.#.
+..###.#.#.
+
+Tile 2971:
+..#.#....#
+#...###...
+#.#.###...
+##.##..#..
+.#####..##
+.#..####.#
+#..#.#..#.
+..####.###
+..#.#.###.
+...#.#.#.#
+
+Tile 2729:
+...#.#.#.#
+####.#....
+..#.#.....
+....#..#.#
+.##..##.#.
+.#.####...
+####.#.#..
+##.####...
+##..#.##..
+#.##...##.
+
+Tile 3079:
+#.#.#####.
+.#..######
+..#.......
+######....
+####.#..#.
+.#...#.##.
+#.#####.##
+..#.###...
+..#.......
+..#.###...
+
+"#;
 
     #[test]
     fn test_part1() {
-        assert_eq!(0.to_string(), *solve_part1(INPUT).to_string());
+        assert_eq!(
+            20899048083289_i64.to_string(),
+            *solve_part1(INPUT).to_string()
+        );
     }
 
     #[test]
