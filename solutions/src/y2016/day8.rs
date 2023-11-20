fb11467779fb39834fb7bec963b75e002c641098
Thomas Barusseau <thomas.barusseau@swan.io>
2023-11-20 22:08:53 +0100

    feat: y2016, day 8

--- solutions/src/y2016/day8.rs
+++ solutions/src/y2016/day8.rs
@@ -1,7 +1,6 @@
-use std::convert::TryFrom;
+use std::{convert::TryFrom, fmt::Display};
 
-use lazy_static::lazy_static;
-use regex::Regex;
+use itertools::Itertools;
 
 use crate::solver::Solver;
 
@@ -15,49 +14,104 @@ enum Instruction {
     RotateColumn(usize, usize),
 }
 
-lazy_static! {
-    static ref RE_RECT: Regex = Regex::new(r"^rect (\d+)x(\d+)$").unwrap();
-    static ref RE_ROTATE_ROW: Regex = Regex::new(r"^rotate column x=(\d+) by (\d+)$").unwrap();
-    static ref RE_ROTATE_COLUMN: Regex = Regex::new(r"^rotate row y=(\d+) by (\d+)$").unwrap();
-}
-
 impl TryFrom<&str> for Instruction {
     type Error = ();
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
-        if RE_RECT.is_match(value) {
-            let captures = RE_RECT.captures(value).ok_or_else(|| ())?;
-
-            let x = usize::from_str_radix(captures.get(1).ok_or_else(|| ())?.as_str(), 10)?;
-            let y = usize::from_str_radix(captures.get(2).ok_or_else(|| ())?.as_str(), 10)?;
-
-            Ok(Instruction::Rect(x, y))
-        } else if RE_ROTATE_ROW.is_match(value) {
-        } else if RE_ROTATE_COLUMN.is_match(value) {
-        } else {
-            Err(())
+        let parsed = sscanf::sscanf!(value, "rect {usize}x{usize}");
+        if let Ok((x, y)) = parsed {
+            return Ok(Instruction::Rect(x, y));
         }
+
+        let parsed = sscanf::sscanf!(value, "rotate row y={usize} by {usize}");
+        if let Ok((x, y)) = parsed {
+            return Ok(Instruction::RotateRow(x, y));
+        }
+
+        let parsed = sscanf::sscanf!(value, "rotate column x={usize} by {usize}");
+        if let Ok((x, y)) = parsed {
+            return Ok(Instruction::RotateColumn(x, y));
+        }
+
+        Err(())
     }
 }
 
 fn process_input(input: &str) -> Vec<Instruction> {
-    input
+    input.lines().flat_map(Instruction::try_from).collect_vec()
 }
 
-#[allow(unused)]
+struct Screen(Vec<Vec<bool>>);
+
+impl Screen {
+    pub fn process_instruction(&mut self, instruction: &Instruction) {
+        match instruction {
+            Instruction::Rect(x, y) => {
+                for j in 0..*y {
+                    for i in 0..*x {
+                        self.0[j][i] = true;
+                    }
+                }
+            }
+            Instruction::RotateRow(x, y) => {
+                self.0[*x].rotate_right(*y);
+            }
+            Instruction::RotateColumn(x, y) => {
+                let mut v = vec![];
+
+                for j in 0..6 {
+                    v.push(self.0[j][*x]);
+                }
+
+                v.rotate_right(*y);
+
+                for j in 0..6 {
+                    self.0[j][*x] = v[j];
+                }
+            }
+        }
+    }
+}
+
+impl Display for Screen {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "\n")?;
+
+        for line in self.0.iter() {
+            write!(
+                f,
+                "{}\n",
+                line.iter()
+                    .map(|&v| if v { '█' } else { ' ' })
+                    .collect::<String>()
+            )?;
+        }
+
+        Ok(())
+    }
+}
+
 fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+    let instructions = process_input(input);
+    let mut screen = Screen(vec![vec![false; 50]; 6]);
 
-    let res = "Part 1 not done";
+    instructions
+        .iter()
+        .for_each(|inst| screen.process_instruction(inst));
+
+    let res = screen.0.iter().flatten().filter(|v| **v).count();
     Box::new(res)
 }
 
-#[allow(unused)]
 fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+    let instructions = process_input(input);
+    let mut screen = Screen(vec![vec![false; 50]; 6]);
 
-    let res = "Part 2 not done";
-    Box::new(res)
+    instructions
+        .iter()
+        .for_each(|inst| screen.process_instruction(inst));
+
+    Box::new(screen)
 }
 
 #[cfg(test)]
