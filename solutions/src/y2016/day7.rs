b1910ac3898e55688f5b27c00d976f8bce2c7038
Thomas BARUSSEAU <thomas.barusseau@swan.io>
2023-11-20 14:06:44 +0100

    feat: y2016, day 7

--- solutions/src/y2016/day7.rs
+++ solutions/src/y2016/day7.rs
@@ -1,8 +1,12 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
+
 use crate::solver::Solver;
 
 pub struct Day7;
 
-crate::impl_day!("7", false);
+crate::impl_day!("7", true);
 
 #[derive(Debug)]
 enum IpSequence {
@@ -10,49 +14,112 @@ enum IpSequence {
     Hypernet(String),
 }
 
-fn process_input(input: &str) -> Vec<Vec<IpSequence>> {
+fn to_ip(input: &str) -> Vec<IpSequence> {
     input
+        .replace("[", " ")
+        .replace("]", " ")
         .trim()
-        .lines()
-        .map(|l| {
-            let mut v = vec![];
-            let mut s = String::new();
-
-            for ch in l.chars() {
-                match ch {
-                    '[' => {
-                        v.push(IpSequence::Regular(s.clone()));
-                        s.clear();
-                    }
-                    ']' => {
-                        v.push(IpSequence::Hypernet(s.clone()));
-                        s.clear();
-                    }
-                    'a'..='z' => s.push(ch),
-                    _ => panic!(),
-                }
+        .split(" ")
+        .enumerate()
+        .map(|(i, s)| {
+            if i % 2 == 0 {
+                IpSequence::Regular(s.to_owned())
+            } else {
+                IpSequence::Hypernet(s.to_owned())
             }
-
-            v.push(IpSequence::Regular(s));
-
-            v
         })
         .collect()
 }
 
+fn process_input(input: &str) -> Vec<Vec<IpSequence>> {
+    input.lines().map(to_ip).collect()
+}
+
+fn is_abba(s: &str) -> bool {
+    for (a, b, c, d) in s.chars().tuple_windows() {
+        if a != b && b == c && a == d {
+            return true;
+        }
+    }
+
+    false
+}
+
 fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
     let input = process_input(input);
-    println!("{:?}", input);
 
-    let res = "Part 1 not done";
+    let res: i32 = input
+        .iter()
+        .map(|sequence| {
+            let regulars = sequence
+                .into_iter()
+                .filter_map(|v| match v {
+                    IpSequence::Regular(r) => Some(r),
+                    _ => None,
+                })
+                .collect_vec();
+
+            let hypernets = sequence
+                .into_iter()
+                .filter_map(|v| match v {
+                    IpSequence::Hypernet(h) => Some(h),
+                    _ => None,
+                })
+                .collect_vec();
+
+            if regulars.iter().any(|r| is_abba(r)) && hypernets.iter().all(|h| !is_abba(h)) {
+                1
+            } else {
+                0
+            }
+        })
+        .sum();
+
     Box::new(res)
 }
 
-#[allow(unused)]
+fn supports_ssl(sequence: &[IpSequence]) -> bool {
+    let regulars = sequence
+        .into_iter()
+        .filter_map(|v| match v {
+            IpSequence::Regular(r) => Some(r),
+            _ => None,
+        })
+        .collect_vec();
+
+    let hypernets = sequence
+        .into_iter()
+        .filter_map(|v| match v {
+            IpSequence::Hypernet(h) => Some(h),
+            _ => None,
+        })
+        .collect_vec();
+
+    let mut aba_collection = HashSet::new();
+
+    regulars.iter().for_each(|r| {
+        for (a, b, c) in r.chars().tuple_windows() {
+            if a != b && a == c && !aba_collection.contains(&(a, b)) {
+                aba_collection.insert((a, b));
+            }
+        }
+    });
+
+    for hyper in hypernets.iter() {
+        for (a, b, c) in hyper.chars().tuple_windows() {
+            if a != b && a == c && aba_collection.contains(&(b, a)) {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
 fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
     let input = process_input(input);
 
-    let res = "Part 2 not done";
+    let res = input.iter().filter(|s| supports_ssl(s)).count();
     Box::new(res)
 }
 
@@ -60,15 +127,21 @@ fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
 mod tests {
     use super::*;
 
-    const INPUT: &str = r#""#;
-
     #[test]
-    fn test_part1() {
-        assert_eq!(0.to_string(), *solve_part1(INPUT).to_string());
+    fn test_abba() {
+        assert!(is_abba("abba"));
+        assert!(is_abba("ioxxoj"));
+        assert!(!is_abba("asdfgh"));
+        assert!(!is_abba("a"));
+        assert!(!is_abba(""));
     }
 
     #[test]
-    fn test_part2() {
-        assert_eq!(0.to_string(), *solve_part2(INPUT).to_string());
+    fn test_ssl() {
+        assert!(supports_ssl(&to_ip("aba[bab]xyz")));
+        assert!(supports_ssl(&to_ip("aaa[kek]eke")));
+        assert!(supports_ssl(&to_ip("zazbz[bzb]cdb")));
+
+        assert!(!supports_ssl(&to_ip("xyx[xyx]xyx")));
     }
 }
