7dc4bdb8e8dfcbe5a0d95b618d4307930536599f
Thomas Barusseau <thomas.barusseau@swan.io>
2023-12-01 12:53:05 +0100

    feat: y2023, day 1 (rewritten)

--- solutions/src/y2023/day1.rs
+++ solutions/src/y2023/day1.rs
@@ -4,90 +4,58 @@ pub struct Day1;
 
 crate::impl_day!("1", true);
 
-fn process_input(input: &str) -> &str {
-    input.trim_end()
+fn find_side_digit(digits: &[&str], line: &str, left: bool) -> usize {
+    let map = digits.iter().enumerate().flat_map(|(i, d)| {
+        if left {
+            if let Some(min_index) = line.find(d) {
+                Some((min_index, i % 9 + 1))
+            } else {
+                None
+            }
+        } else {
+            if let Some(max_index) = line.rfind(d) {
+                Some((max_index, i % 9 + 1))
+            } else {
+                None
+            }
+        }
+    });
+
+    let extreme = if left {
+        map.min_by(|(i1, _), (i2, _)| i1.cmp(i2))
+    } else {
+        map.max_by(|(i1, _), (i2, _)| i1.cmp(i2))
+    };
+
+    extreme.map(|(_, v)| v).unwrap()
 }
 
-fn compute_result(input: &str) -> u32 {
+fn compute_solution(input: &str, digits: &[&str]) -> usize {
     input
-        .replace(char::is_alphabetic, "")
+        .trim_end()
         .lines()
-        .map(|l| {
-            let mut iter = l.chars().flat_map(|c| char::to_digit(c, 10)).peekable();
+        .map(|line| {
+            let min = find_side_digit(digits, line, true);
+            let max = find_side_digit(digits, line, false);
 
-            let first = *iter.peek().expect("no first numerical character");
-            let last = iter.last().expect("no last numerical character");
-
-            first * 10 + last
+            min * 10 + max
         })
         .sum()
 }
 
 fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+    let digits = &["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-    let res = compute_result(input);
-
-    Box::new(res)
-}
-
-fn replace_spelled_out_digits(input: &str) -> String {
-    let mut ret = String::new();
-    let mut slice = input;
-
-    let numerals = vec![
-        ("one", '1'),
-        ("two", '2'),
-        ("three", '3'),
-        ("four", '4'),
-        ("five", '5'),
-        ("six", '6'),
-        ("seven", '7'),
-        ("eight", '8'),
-        ("nine", '9'),
-    ];
-
-    loop {
-        let len = slice.len();
-
-        if len < 3 {
-            ret.push_str(slice);
-
-            break;
-        }
-
-        let mut found = false;
-        for (s, c) in numerals.iter() {
-            if slice.starts_with(s) {
-                ret.push(*c);
-                found = true;
-                break;
-            }
-        }
-
-        if !found {
-            // For simplicity sake, discard anything that we don't want.
-            let next_char = slice.chars().next().expect("no next char");
-
-            if next_char.is_numeric() || next_char == '\n' {
-                ret.push(next_char);
-            }
-        }
-
-        // Advance the slice char by char to make sure we don't miss cases where
-        // the last letters of a numeral form the first letters of another one.
-        slice = &slice[1..];
-    }
-
-    ret
+    Box::new(compute_solution(input, digits))
 }
 
 fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
-    let replaced = replace_spelled_out_digits(input);
-    let res = compute_result(&replaced);
+    let digits = &[
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six",
+        "seven", "eight", "nine",
+    ];
 
-    Box::new(res)
+    Box::new(compute_solution(input, digits))
 }
 
 #[cfg(test)]
