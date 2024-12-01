e6544c96e9b87b0dabbc587ed429882b456b3b40
Thomas Barusseau <thomas.barusseau@thetribe.io>
2024-12-01 09:45:43 +0100

    feat: y2024, day1

--- solutions/src/y2024/day1.rs
+++ solutions/src/y2024/day1.rs
@@ -1,24 +1,44 @@
+use num::abs;
+
 pub struct Day1;
 
-crate::impl_day!("1", false);
+crate::impl_day!("1", true);
 
-fn process_input(input: &str) -> &str {
+fn process_input(input: &str) -> (Vec<i32>, Vec<i32>) {
+    let mut l_left = vec![];
+    let mut l_right = vec![];
+
     input
+        .trim()
+        .lines()
+        .map(|l| l.split("   "))
+        .for_each(|mut s| {
+            l_left.push(s.next().unwrap().parse::<i32>().unwrap());
+            l_right.push(s.next().unwrap().parse::<i32>().unwrap());
+        });
+
+    (l_left, l_right)
 }
 
-#[allow(unused)]
 fn solve_part1(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+    let (mut left, mut right) = process_input(input);
 
-    let res = "Part 1 not done";
+    left.sort_unstable();
+    right.sort_unstable();
+
+    let res: i32 = left.iter().zip(&right).map(|(a, b)| abs(b - a)).sum();
+
     Box::new(res)
 }
 
-#[allow(unused)]
 fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
-    let input = process_input(input);
+    let (left, right) = process_input(input);
 
-    let res = "Part 2 not done";
+    let res: i32 = left
+        .iter()
+        .map(|&a| a * right.iter().filter(|&&b| b == a).count() as i32)
+        .sum();
+
     Box::new(res)
 }
 
@@ -26,15 +46,20 @@ fn solve_part2(input: &str) -> Box<dyn std::fmt::Display> {
 mod tests {
     use super::*;
 
-    const INPUT: &str = r"";
+    const INPUT: &str = r"3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
 
     #[test]
     fn test_part1() {
-        assert_eq!(0.to_string(), *solve_part1(INPUT).to_string());
+        assert_eq!(11.to_string(), *solve_part1(INPUT).to_string());
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0.to_string(), *solve_part2(INPUT).to_string());
+        assert_eq!(31.to_string(), *solve_part2(INPUT).to_string());
     }
 }
